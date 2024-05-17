bdebbcab29b37eccdf0d7e1bf9be378ac420e20a
WilliamFitzjohn <william_fitzjohn@icloud.com>
2024-05-17 19:38:54 -0400

    docker context

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export DOCKER_CONTEXT=dietpi
-
 LOCK_FILE="$(pwd)/PDFtools.lock"
 cd /home/dietpi/Applications/PDFtools
 flock -n $LOCK_FILE ./scripts/change-detector.sh >> /home/dietpi/Logs/deploy-pdftools.log 2>&1
